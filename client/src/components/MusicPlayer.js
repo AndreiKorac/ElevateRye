d92bccdea2eb33297612ed906c5b7d2f1931a2d9
Christopher Seow <christopherseow456@gmail.com>
2018-09-23 00:25:31 -0400

    Music player based on weather data implementation

--- client/src/components/MusicPlayer.js
+++ client/src/components/MusicPlayer.js
@@ -1,22 +1,46 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import styled from 'styled-components';
 
-class MusicPlayer extends Component{
-	constructor(props) {
-        super(props);
+const EmbedSpotify = styled.iframe`
+	display: block;
+	margin: auto;
+`;
 
-        this.state = {
-            
-        };
+export default class MusicPlayer extends Component{
+
+    renderPlayer = () => {
+        console.log("test")
+        let playlist;
+        let temp = this.props.weatherData.weather[0].main;
+        if (temp === "Clear") {
+            playlist = "37i9dQZF1DX5Ozry5U6G0d";
+            console.log("CLEAR");
+        } else if(temp == "Clouds") {
+            playlist = "5NZaOHX4B7WXMpin3plCzF";
+            console.log("Clouds");
+        } else if(temp == "Snow") {
+            playlist = "37i9dQZF1DX6R7QUWePReA";
+            console.log("Snow");
+        } else if(temp == "Thunderstorm") {
+            playlist = "1rqfUOM7dVvfSQ9TIT1fu5";
+            console.log("Thunder");
+        } else if(temp == "Drizzle") {
+            playlist = "37i9dQZF1DX9sIqqvKsjG8";
+            console.log("Drizzle");
+        } else if(temp == "Rain") {
+            playlist = "37i9dQZF1DX2UgsUIg75Vg";
+            console.log("Rain");
+        } else {
+            playlist = "37i9dQZF1DXarlH9zHuV0v";
+            console.log("Atmosphere (Fog and Mist)");
+        }
+
+        return <EmbedSpotify src={"https://open.spotify.com/embed/user/spotify/playlist/" + playlist} width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></EmbedSpotify>;
     }
-	
-	render(){
-		return(
-			
-			<iframe src="https://open.spotify.com/playlist/37i9dQZF1DWVu0D7Y8cYcs" width="300" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-			
-		);
-	}
-}
 
-export default MusicPlayer;
+    render() {
+        return (
+            <div>{this.renderPlayer()}</div>
+        );
+    }
+}
